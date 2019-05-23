124f1a2d8734fabcc90f525600a4d13b2c28fe6e
Brian Hurst <brian.hurst@gsa.gov>
2019-05-23 12:47:35 -0400

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React, { Component } from "react";
 import styled from "styled-components/macro";
 import tw from "tailwind.macro";
 
-// use tailwind classes the styled way
 const Header = styled.header`
-  ${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl text-white`};
+  ${tw`min-h-screen flex flex-col items-center justify-center`};
 `;
 
 class App extends Component {
   render() {
-    // or inline via fancy css prop
     return (
-      <div css={tw`text-center`}>
-        <Header>
-          <p css={tw`text-blue-light`}>
-            Using <code>tailwind</code> and <code>styled-components</code>{" "}
-            together.
-          </p>
-        </Header>
-      </div>
+      <Header>
+        <h1 css={tw`font-sans text-xl text-gray-800 text-center`}>
+          TreatmentLocator.gov
+        </h1>
+      </Header>
     );
   }
 }
