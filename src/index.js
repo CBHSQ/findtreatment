fb7ac6cacb59b3147bd6bd42033a813829d2cef9
Brian Hurst <brian.hurst@gsa.gov>
2019-05-28 12:59:26 -0400

    Add BASEURL to react router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.REACT_APP_FEDERALIST_BASEURL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
