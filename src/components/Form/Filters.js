bc957115c4c74f293f65235959307bf5e3b8d6d1
Allison Norman <allison.norman@gsa.gov>
2019-07-02 09:37:44 -0700

    Removing opioids from MAT label

--- src/components/Form/Filters.js
+++ src/components/Form/Filters.js
@@ -145,7 +145,7 @@ export class Filters extends Component {
             <Row>
               <Field
                 name="mat"
-                label="Type of medication-assisted treatment (MAT) for opioids"
+                label="Type of medication-assisted treatment (MAT)"
                 plural="treatments"
                 component={Select}
                 options={filterOptions.mat}
