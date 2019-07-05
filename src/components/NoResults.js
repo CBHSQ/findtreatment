450a368a18a81a9b3e6dc9910ed9c888ebde0dd4
Allison Norman <allison.norman@gsa.gov>
2019-07-05 15:50:05 -0700

    Playing with formatting of no results messaging

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -8,11 +8,14 @@ const NoResults = () => {
   return (
     <div css={tw`shadow border rounded p-6 text-center mb-6`}>
       <h2 css={tw`mb-6`}>
-        <span css={tw`block font-bold text-3xl`}>This search didn't find any providers near you.</span>Try expanding your search by applying less filters, like your ZIP code and the type of treatment you need.
+        <span css={tw`block font-bold text-2xl mb-6`}>We couldn't find any providers near you.</span>
       </h2>
       <div css={tw`max-w-md mx-auto`}>
         <p css={tw`mb-6`}>
-          If you need assistance, our national helpline is available 24/7 and can assist you with treatment referrals and information. Call <a href="tel:+1-800-662-4357">+1-800-662-HELP (4357)</a> to speak to someone.
+          Try expanding your search by applying less filters, like your ZIP code and the type of treatment you need.
+        </p>
+        <p css={tw`mb-6`}>
+          If you need assistance, our national helpline is available 24/7 and can assist you with treatment referrals and information. Call <a href="tel:+1-800-662-4357">1-800-662-HELP (4357)</a> to speak to someone.
         </p>
       </div>
     </div>
