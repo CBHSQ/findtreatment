4a640f47e484a3323340366fbdf705a997ff83fa
Brian Hurst <brian.hurst@gsa.gov>
2019-07-10 11:55:11 -0400

    Adds beta banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'styled-components/macro';
+import tw from 'tailwind.macro';
 import HelpLine from './HelpLine';
 import Banner from './Banner';
 import Nav from './Nav';
@@ -6,8 +8,22 @@ import Nav from './Nav';
 const Header = () => {
   return (
     <>
-      <Banner />
-      <HelpLine />
+      <div css={tw`bg-yellow-400`}>
+        <div className="container" css={tw`py-4 font-bold`}>
+          We are testing a new design for our treatment locator. Have feedback?{' '}
+          <a css={tw`underline`} href="https://forms.gle/35ZHQCGBkxCsJcs78">
+            We want to hear from you.
+          </a>
+        </div>
+      </div>
+      <div css={tw`bg-gray-200`}>
+        <div
+          css={tw`lg:relative lg:mx-auto max-w-full lg:max-w-5xl xl:max-w-7xl`}
+        >
+          <Banner />
+          <HelpLine />
+        </div>
+      </div>
       <Nav />
     </>
   );
