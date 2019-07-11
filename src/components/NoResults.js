44b73d0bc8129f5777ab463e8db04a139e814d7c
Brian Hurst <brian.hurst@gsa.gov>
2019-07-11 15:23:24 -0400

    Update error message

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import 'styled-components/macro';
 import tw from 'tailwind.macro';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const NoResults = () => {
   return (
     <div css={tw`shadow border rounded p-6 text-center mb-6`}>
       <h2 css={tw`mb-6`}>
-        <span css={tw`block font-bold text-2xl mb-6`}>We couldn't find any providers near you.</span>
+        <span css={tw`block font-bold text-2xl mb-6`}>
+          We couldn't find any providers near you.
+        </span>
       </h2>
       <div css={tw`max-w-md mx-auto`}>
         <p css={tw`mb-6`}>
-          Try expanding your search by applying fewer filters,<br /> like only your ZIP code and payment option.
+          Try expanding your search by applying fewer filters,
+          <br /> like only your ZIP code and payment option.
         </p>
         <p css={tw`mb-6`}>
-          If you need assistance, our national helpline is available 24/7 and can assist you with treatment referrals and information. Call <a href="tel:+1-800-662-4357">1-800-662-HELP (4357)</a> to speak to someone.
+          If you need assistance, our national helpline is available 24/7 and
+          can assist you with treatment referrals and information. Call{' '}
+          <a href="tel:+1-800-662-4357">1-800-662-HELP (4357)</a> to speak to
+          someone.
         </p>
       </div>
     </div>
