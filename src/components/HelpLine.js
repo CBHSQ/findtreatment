74d10cab39f957b9328a4d1fb2490054e367b38f
Brian Hurst <brian.hurst@gsa.gov>
2019-07-16 15:15:40 -0400

    Update banner colors

--- src/components/HelpLine.js
+++ src/components/HelpLine.js
@@ -10,9 +10,9 @@ const HelpLine = () => {
     <OutboundLink
       eventLabel="Header Helpline link"
       to="tel:18006624357"
-      css={tw`w-full lg:w-auto lg:absolute lg:right-0 lg:top-0 lg:mr-6 bg-red-700 text-white hover:text-white text-sm lg:rounded-b flex items-center lg:items-stretch justify-center lg:justify-start shadow-md`}
+      css={tw`w-full lg:w-auto lg:absolute lg:right-0 lg:top-0 lg:mr-6 bg-teal-700 text-white hover:text-white text-sm lg:rounded-b flex items-center lg:items-stretch justify-center lg:justify-start shadow-md`}
     >
-      <div css={tw`lg:bg-red-800 py-2 lg:px-4 lg:rounded-bl`}>
+      <div css={tw`lg:bg-teal-800 py-2 lg:px-4 lg:rounded-bl`}>
         <FontAwesomeIcon icon={faPhone} css={tw`fill-current w-4 h-4`} />
       </div>
       <div css={tw`py-2 px-2 lg:px-4`}>
