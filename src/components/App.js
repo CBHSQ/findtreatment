e609cd0b6a9ab896eed226524959a93d6c0586cd
Brian Hurst <brian.hurst@gsa.gov>
2019-07-18 10:28:20 -0400

    Fix test, update App to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import 'styled-components/macro';
+import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
 import Header from './Header';
 import Home from './Home';
@@ -10,22 +10,24 @@ import Page from './Page';
 import NoMatch from './NoMatch';
 import Footer from './Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div css={tw`font-sans text-gray-900 leading-normal`}>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/results" component={Results} />
-          <Route path="/details" component={Details} />
-          <Route path="/content/:pageId" component={Page} />
-          <Route component={NoMatch} />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
-}
+const StyledApp = styled.div`
+  ${tw`font-sans text-gray-900 leading-normal`}
+`;
+
+const App = () => {
+  return (
+    <StyledApp>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/results" component={Results} />
+        <Route path="/details" component={Details} />
+        <Route path="/content/:pageId" component={Page} />
+        <Route component={NoMatch} />
+      </Switch>
+      <Footer />
+    </StyledApp>
+  );
+};
 
 export default App;
