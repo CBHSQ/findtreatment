798307c853f799ce0dffe113c9cb40f64b21614c
Brian Hurst <brian.hurst@gsa.gov>
2019-07-26 16:59:17 -0400

    Add helpline number to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const DEFAULT_DISTANCE = 16093.4; // Radius from lat/long in meters
 export const DEFAULT_PAGE_SIZE = 10; // Results returned with each request
 export const DEFAULT_LIMIT_TYPE = 2; // 0 = State, 1 = County, 2 = Distance
 export const DEFAULT_SORT = 0; // 0 = Distance: Low to High
+export const HELPLINE_TEXT = '1-800-662-HELP (4357)';
+export const HELPLINE_LINK = '+1-800-662-4357';
