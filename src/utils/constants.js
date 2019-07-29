8c739b7c38cb25a6de6e0eb9ff1c222f3f54d370
Brian Hurst <brian.hurst@gsa.gov>
2019-07-29 13:18:47 -0400

    Change default search radius (25mi)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const ADVANCED_FILTERS = ['age', 'language', 'VET', 'GL', 'mat'];
-export const DEFAULT_DISTANCE = 16093.4; // Radius from lat/long in meters
+export const DEFAULT_DISTANCE = 40233.6; // Radius from lat/long in meters
 export const DEFAULT_PAGE_SIZE = 10; // Results returned with each request
 export const DEFAULT_LIMIT_TYPE = 2; // 0 = State, 1 = County, 2 = Distance
 export const DEFAULT_SORT = 0; // 0 = Distance: Low to High
