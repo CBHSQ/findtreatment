17b01084e576cb3cabd44181f5c762653ed7827d
Joe Krzystan <joseph.krzystan@gsa.gov>
2019-08-02 10:52:25 -0400

    Use helpline number defined in constants

--- src/utils/warnings.js
+++ src/utils/warnings.js
@@ -1,2 +1,3 @@
-export const LOCATION_WARNING =
-  'Please enter a city or postal code and select one of the results. If you need help finding treatment, call us 1-800-662-HELP (4357)';
+import { HELPLINE_TEXT } from './constants';
+
+export const LOCATION_WARNING = `Please enter a city or postal code and select one of the results. If you need help finding treatment, call us ${HELPLINE_TEXT}`;
