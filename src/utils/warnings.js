e794cae2fc78ade3ff7fb7e1e3f299d57bdf109c
Joe Krzystan <joseph.krzystan@gsa.gov>
2019-08-05 15:35:44 -0400

    Simplify location search input warning

--- src/utils/warnings.js
+++ src/utils/warnings.js
@@ -1,3 +1,1 @@
-import { HELPLINE_TEXT } from './constants';
-
-export const LOCATION_WARNING = `Please enter a city or postal code and select one of the results. If you need help finding treatment, call us ${HELPLINE_TEXT}`;
+export const LOCATION_WARNING = 'Please enter a city or zip code';
