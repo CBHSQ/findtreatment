bd0b533a07068be6e829d3033877840d8bb53b97
Brian Hurst <brian.hurst@gsa.gov>
2019-08-13 16:56:51 -0400

    Remove logic to conditionally set service type in endpoint request

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const DEFAULT_DISTANCE = 25 * metersPerMile; // Radius from lat/long in m
 export const DEFAULT_PAGE_SIZE = 10; // Results returned with each request
 export const DEFAULT_LIMIT_TYPE = 2; // 0 = State, 1 = County, 2 = Distance
 export const DEFAULT_SORT = 0; // 0 = Distance: Low to High
-export const DEFAULT_STYPE = 'BOTH';
+export const DEFAULT_STYPE = 'SA';
 export const HELPLINE_TEXT = '1-800-662-HELP (4357)';
 export const HELPLINE_LINK = '+1-800-662-4357';
 export const SITE_TITLE = 'Treatment Finder';
