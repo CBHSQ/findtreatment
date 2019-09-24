a3afd9d8d7bf05f889f9519c22693c829bb0d76a
David Corwin <david.corwin@gsa.gov>
2019-09-24 09:54:23 -0700

    Replace pngs with highly optimized jpgs

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,10 +8,10 @@ import { HashLink } from 'react-router-hash-link';
 
 import content from '../utils/content';
 import { handleReceiveFacilities } from '../actions/facilities';
-import mobileBackground from '../images/film-strip_mobile.png';
-import mobileBackground_2x from '../images/film-strip_mobile@2x.png';
-import backgroundLeft_2x from '../images/film-strip_l@2x.png';
-import backgroundRight_2x from '../images/film-strip_r@2x.png';
+import mobileBackground from '../images/film-strip_mobile.jpg';
+import mobileBackground_2x from '../images/film-strip_mobile@2x.jpg';
+import backgroundLeft_2x from '../images/film-strip_l@2x.jpg';
+import backgroundRight_2x from '../images/film-strip_r@2x.jpg';
 
 import ScreenContext from './ScreenContext';
 import FormHomepage from './Form/FormHomepage';
