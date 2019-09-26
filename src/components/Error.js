c25dcef3e5a990825bc3a98aa661387a0069668c
David Corwin <david.corwin@gsa.gov>
2019-09-26 12:48:50 -0700

    Refactor `NoMatch` to use `Error` component, add a test!!

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 import tw from 'tailwind.macro';
 import 'styled-components/macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -18,26 +19,24 @@ class Error extends Component {
   }
 
   render() {
+    const { title, headerText, description, helpLabel } = this.props;
+
     return (
       <div css={tw`mx-auto p-6 mb-6 text-center`}>
         <Helmet>
-          <title>Error</title>
+          <title>{title}</title>
         </Helmet>
         <h1
           css={tw`text-3xl font-bold mb-10`}
           ref={this.headerRef}
           tabIndex="-1"
         >
-          Something's wrong on our side.
+          {headerText}
         </h1>
         <div css={tw`max-w-xl mx-auto`}>
-          <p css={tw`text-xl mb-6`}>
-            We're having technical issues right now. The problem should be fixed
-            soon, and in the meantime SAMHSA's national helpline is available
-            24/7 and can assist you with treatment referrals and information.
-          </p>
+          <p css={tw`text-xl mb-6`}>{description}</p>
           <Button
-            eventLabel="Helpline link from error page"
+            eventLabel={helpLabel}
             to={`tel:${HELPLINE_LINK}`}
             as={OutboundLink}
             primary
@@ -50,5 +49,21 @@ class Error extends Component {
     );
   }
 }
+
+Error.propTypes = {
+  title: PropTypes.string,
+  headerText: PropTypes.string,
+  description: PropTypes.string,
+  helpLabel: PropTypes.string
+};
+
+Error.defaultProps = {
+  title: 'Error',
+  headerText: 'Something is wrong on our side.',
+  description: `We're having technical issues right now. The problem should be fixed
+  soon, and in the meantime SAMHSA's national helpline is available
+  24/7 and can assist you with treatment referrals and information.`,
+  helpLabel: 'Helpline link from error page'
+};
 
 export default Error;
