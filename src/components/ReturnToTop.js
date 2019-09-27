e94a3441411da334968c5733cdf4d1bfdb694be5
David Corwin <david.corwin@gsa.gov>
2019-09-27 14:27:46 -0700

    Use HashLink component

--- src/components/ReturnToTop.js
+++ src/components/ReturnToTop.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 import tw from 'tailwind.macro';
 import 'styled-components/macro';
 
 const ReturnToTop = () => {
   return (
     <div css={tw`pb-4 sm:hidden`} className="container">
-      <a href="#top" aria-label="Return to top" css={tw`text-blue`}>
+      <HashLink to="/#top" aria-label="Return to top" css={tw`text-blue`}>
         Return to top
-      </a>
+      </HashLink>
     </div>
   );
 };
