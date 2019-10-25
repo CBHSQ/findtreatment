687fd3786b675ab7a74871adce89f3b93aed7711
David Corwin <david.corwin@gsa.gov>
2019-10-25 15:04:39 -0700

    Set og:url equal to root url for 404 pages

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import Error from './Error';
 
 const NoMatch = () => (
-  <Error
-    title="Not found"
-    headerText="This page isn’t here, but we can get you help."
-    description="SAMHSA's national helpline is available 24/7 and can assist you with treatment referrals and information."
-  />
+  <>
+    <Helmet>
+      <meta property="og:url" content={process.env.REACT_APP_SITE_DOMAIN} />
+    </Helmet>
+    <Error
+      title="Not found"
+      headerText="This page isn’t here, but we can get you help."
+      description="SAMHSA's national helpline is available 24/7 and can assist you with treatment referrals and information."
+    />
+  </>
 );
 
 export default NoMatch;
